89cab97c94ce1e69075eca2429b37001679c8278
amyma17 <amyma171311@gmail.com>
2018-06-19 15:32:07 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,14 +1,18 @@
 def breakfast(food="frosted flakes")
   puts "Morning is a good time for #{food}!"
 end
+
 def lunch(meal= "grilled cheese")
   puts "#{meal} makes a perfect lunch"
 end
+
 def snacks(food="Cheetos")
   "Any time, is the right time for #{food}!"
 end
+
 def dinner(food="salmon")
   puts "For dinner #{meal} would be a good option."
+end
 breakfast
 lunch
 snacks
