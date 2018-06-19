42f4e86adcfd2d54125b32faee68868297abcf3f
amyma17 <amyma171311@gmail.com>
2018-06-19 15:35:41 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -2,18 +2,24 @@ def breakfast(food="frosted flakes")
   puts "Morning is a good time for #{food}!"
 end
 
-def lunch(meal= "grilled cheese")
-  puts "#{meal} makes a perfect lunch"
+def morning snack(food="fruit")
+  puts "If you're hungry before lunch, you should eat some #{food}."
 end
 
-def snacks(food="Cheetos")
-  "Any time, is the right time for #{food}!"
+def lunch(meal= "grilled cheese")
+  puts "#{meal} makes a perfect lunch."
+end
+
+def snack (snack=cheetos)
+  puts "Have some #{snack} for a snack"
 end
 
 def dinner(meal="salmon")
   puts "For dinner #{meal} would be a good option."
 end
+
 breakfast
+morning snack
 lunch
-snacks
+snack
 dinner
